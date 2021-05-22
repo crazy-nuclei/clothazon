40f283d487953c889ea1ef40db228225a3cf5e67
Aayush <aayush.551a@gmail.com>
2021-05-22 17:06:26 +0530

    added addItemToCart feature

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
+const CollectionItem = ({ item, addItem }) => {
 
-const CollectionItem = ({ name, price, imageUrl }) => {
-
+    const { imageUrl, name, price } = item;
     return (
         <div className="collection-item">
             <div
@@ -16,8 +19,13 @@ const CollectionItem = ({ name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)} >ADD TO CART</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
